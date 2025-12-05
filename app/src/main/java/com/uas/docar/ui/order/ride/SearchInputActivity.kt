4b97889b35fa5a1ecd0afc863636bdbca07a616f
poenzy <poenzyozzi@gmail.com>
2025-12-05 15:08:57 +0700

    Home Activity

--- app/src/main/java/com/uas/docar/ui/order/ride/SearchInputActivity.kt
+++ app/src/main/java/com/uas/docar/ui/order/ride/SearchInputActivity.kt
@@ -1,5 +1,53 @@
 package com.uas.docar.ui.order.ride
 
-class SearchInputActivity() {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.uas.docar.databinding.ActivitySearchInputBinding
+import com.uas.docar.ui.adapter.SearchHistoryAdapter // <-- Adapter Riwayat akan dibuat
 
+class SearchInputActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySearchInputBinding
+
+    private val searchHistory = listOf(
+        "Politeknik Negeri Madiun",
+        "Alun-Alun Madiun",
+        "Stasiun Madiun",
+        "Rumah Sakit Soedono"
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySearchInputBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnBack.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        setupHistoryRecyclerView()
+
+        binding.etDestinationInput.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus && binding.etDestinationInput.text.isNotEmpty()) {
+                navigateToOrderConfirmation()
+            }
+        }
+    }
+
+    private fun setupHistoryRecyclerView() {
+        val adapter = SearchHistoryAdapter(searchHistory) { location ->
+            binding.etDestinationInput.setText(location)
+            navigateToOrderConfirmation()
+        }
+
+        binding.rvHistory.layoutManager = LinearLayoutManager(this)
+        binding.rvHistory.adapter = adapter
+    }
+
+    private fun navigateToOrderConfirmation() {
+        val intent = Intent(this, OrderConfirmationActivity::class.java)
+        startActivity(intent)
+    }
 }
