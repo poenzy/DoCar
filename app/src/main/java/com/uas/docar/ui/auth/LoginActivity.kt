c546e0016150f1250d3eba0f0cfdb893731bb9e5
111ndy <rindyagustina413@gmail.com>
2025-12-05 13:03:25 +0700

    Merapikan COL setelah pull

--- app/src/main/java/com/uas/docar/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/uas/docar/ui/auth/LoginActivity.kt
@@ -1,4 +1,49 @@
 package com.uas.docar.ui.auth
 
-class LoginActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.uas.docar.databinding.ActivityLoginBinding
+import com.uas.docar.ui.home.HomeActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnMasuk.setOnClickListener {
+            handleLogin()
+        }
+
+        binding.tvDaftar.setOnClickListener {
+            navigateToRegister()
+        }
+    }
+
+    private fun handleLogin() {
+
+        val username = binding.etUsername.text.toString().trim()
+        val password = binding.etPassword.text.toString().trim()
+
+        if (username.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Mohon isi Username dan Password.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        Toast.makeText(this, "Login berhasil!", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun navigateToRegister() {
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
+    }
 }
